e0e1ae01ff7a59629260feb4ae1f9c4734b42141
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-09-01 16:29:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+status="";
 img="";
 function preload(){
     img=loadImage('dog_cat.jpg');
@@ -5,7 +6,22 @@ function preload(){
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
 }
+function modelLoaded(){
+    console.log("model loaded!!");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+
+
 function draw(){
     image(img,0,0,640,420);
     fill("#0000FF");
@@ -13,4 +29,10 @@ function draw(){
     noFill();
     stroke("#0000FF")
     rect(30,50,450,350);
+
+    fill("#0000FF");
+    text('Cat',275,65,15,15);
+    noFill();
+    stroke("#0000FF");
+    rect(270,60,350,350);
 }
